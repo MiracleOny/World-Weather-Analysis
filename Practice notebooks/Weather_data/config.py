9aa711d814bc222d6f90065264c0c8e6b351a700
Miracle Meijs-Onyeoziri <112508685+MiracleOny@users.noreply.github.com>
2024-01-25 11:56:24 -0500

    Delete Practice notebooks/Weather_data/config.py

--- Practice notebooks/Weather_data/config.py
+++
@@ -1,8 +0,0 @@
-# Weather API Key
-weather_api_key="9c8346b4a503fe9b2ca716e7bacaeb06"
-
-# Geoapify API Key
-geoapify_key="fdefb5b1ac4d453896f82d29aa11746f"
-
-# Google Mapping API Key
-gmpapi_key="AIzaSyCCLuAYUk7N9POx2-0Y-vEMwJc3tH6mWn0"
